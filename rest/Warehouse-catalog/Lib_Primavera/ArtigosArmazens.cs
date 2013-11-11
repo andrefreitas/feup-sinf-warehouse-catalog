a08d7ca82a5e74c8b31bfe348fc895d0a9b838fc
André Freitas <p.andrefreitas@gmail.com>
2013-11-11 11:16:28 +0000

    ArtigosArmazens OPTIMIZED

--- rest/Warehouse-catalog/Lib_Primavera/ArtigosArmazens.cs
+++ rest/Warehouse-catalog/Lib_Primavera/ArtigosArmazens.cs
@@ -29,37 +29,43 @@ namespace Warehouse_catalog.Lib_Primavera
 
                 //objList = PriEngine.Engine.Comercial.Artigos.LstArtigos();
 
-                objList = PriEngine.Engine.Consulta("SELECT Artigo, Armazem, StkActual FROM  ARTIGOARMAZEM");
+                string query = "SELECT ARTIGO.Artigo AS Artigo, ARTIGO.Descricao AS ArtigoDesc, ARTIGOMOEDA.PVP1 AS Preco, ARTIGO.Iva AS Iva, FAMILIAS.Descricao AS Familia, "
+                            + "ARTIGO.STKActual AS ArtStk, ARMAZENS.Armazem AS Armazem, ARMAZENS.Descricao AS ArmazemDesc, ARMAZENS.Morada AS Morada, ARMAZENS.Localidade AS Localidade, "
+                            + "ARMAZENS.Cp AS Cp, ARMAZENS.CpLocalidade AS CpLocalidade, ARMAZENS.Telefone AS Telefone, ARMAZENS.Fax AS Fax, ARMAZENS.Distrito AS Distrito, "
+                            + "ARMAZENS.Pais AS Pais, ARTIGOARMAZEM.StkActual AS ArmStk "
+                            + "FROM ARTIGO, ARTIGOARMAZEM, ARTIGOMOEDA, ARMAZENS, "
+                            + "FAMILIAS WHERE "
+                            + "ARTIGO.Artigo = ARTIGOARMAZEM.Artigo AND ARMAZENS.Armazem = ARTIGOARMAZEM.Armazem AND "
+                            + "ARTIGO.Artigo = ARTIGOMOEDA.Artigo AND FAMILIAS.Familia = ARTIGO.Familia";
+
+                objList = PriEngine.Engine.Consulta(query);
 
                 while (!objList.NoFim())
                 {
                     artigo_armazem = new Model.ArtigoArmazem();
 
-                    artigo = Artigos.GetArtigo(objList.Valor("Artigo"));
-                    armazem = Armazens.GetArmazem(objList.Valor("Armazem"));
+                    //artigo = Artigos.GetArtigo(objList.Valor("Artigo"));
+                    //armazem = Armazens.GetArmazem(objList.Valor("Armazem"));
 
 
-                    artigo_armazem.CodArtigo = artigo.CodArtigo;
-                    artigo_armazem.DescArtigo = artigo.Descricao;
-                    artigo_armazem.Preco = artigo.Preco;
-                    artigo_armazem.IVA = artigo.IVA;
-                    artigo_armazem.Familia = artigo.Familia;
-                    artigo_armazem.Imagem = artigo.Imagem;
-                    artigo_armazem.StkAtual = artigo.StkAtual;
-                    artigo_armazem.DescricaoImg = artigo.DescricaoImg;
+                    artigo_armazem.CodArtigo = objList.Valor("Artigo");
+                    artigo_armazem.DescArtigo = objList.Valor("ArtigoDesc");
+                    artigo_armazem.Preco = objList.Valor("Preco");
+                    artigo_armazem.IVA = objList.Valor("Iva");
+                    artigo_armazem.Familia = objList.Valor("Familia");
+                    artigo_armazem.StkAtual = objList.Valor("ArtStk");
 
-                    artigo_armazem.CodArmazem = armazem.CodArmazem;
-                    artigo_armazem.DescArmazem = armazem.Descricao;
-                    artigo_armazem.Morada = armazem.Morada;
-                    artigo_armazem.Localidade = armazem.Localidade;
-                    artigo_armazem.CodPostal = armazem.CodPostal;
-                    artigo_armazem.CodPostalLocalidade = armazem.CodPostalLocalidade;
-                    artigo_armazem.Telefone = armazem.Telefone;
-                    artigo_armazem.Fax = armazem.Fax;
-                    artigo_armazem.Distrito = armazem.Distrito;
-                    artigo_armazem.Pais = armazem.Pais;
-
-                    artigo_armazem.StkArmazem = objList.Valor("StkActual");
+                    artigo_armazem.CodArmazem = objList.Valor("Armazem");
+                    artigo_armazem.DescArmazem = objList.Valor("ArmazemDesc");
+                    artigo_armazem.Morada = objList.Valor("Morada");
+                    artigo_armazem.Localidade = objList.Valor("Localidade");
+                    artigo_armazem.CodPostal = objList.Valor("Cp");
+                    artigo_armazem.CodPostalLocalidade = objList.Valor("CpLocalidade");
+                    artigo_armazem.Telefone = objList.Valor("Telefone");
+                    artigo_armazem.Fax = objList.Valor("Fax");
+                    artigo_armazem.Distrito = objList.Valor("Distrito");
+                    artigo_armazem.Pais = objList.Valor("Pais");
+                    artigo_armazem.StkArmazem = objList.Valor("ArmStk");
 
                     listArtigosArmazens.Add(artigo_armazem);
                     objList.Seguinte();
